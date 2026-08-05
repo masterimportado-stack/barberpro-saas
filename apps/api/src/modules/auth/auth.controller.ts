b2d3caf330cb8e002f0909b26fab75f2d9a2a330
masterimportado-stack <masterimportado@gmail.com>
2026-08-05 12:17:48 -0300

    feat: add auth endpoints skeleton

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  login(@Body() data: LoginDto) {
+    return data;
+  }
+
+  @Post('register')
+  register(@Body() data: RegisterDto) {
+    return data;
+  }
+}
