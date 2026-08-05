45e01199b624fc6511d5b103116de0f18fa975c0
masterimportado-stack <masterimportado@gmail.com>
2026-08-05 12:15:53 -0300

    feat: add auth service base

---
+++ apps/api/src/modules/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  async validateUser(email: string, password: string) {
+    return { email, authenticated: true };
+  }
+}
