1bb1c0de771a6f358b48298ea4f3360ebc889716
masterimportado-stack <masterimportado@gmail.com>
2026-08-05 12:21:59 -0300

    feat: connect auth controller endpoints

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   login(@Body() data: LoginDto) {
-    return data;
+    return this.authService.login(data);
   }
 
   @Post('register')
   register(@Body() data: RegisterDto) {
-    return data;
+    return this.authService.register(data);
   }
 }
